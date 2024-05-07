99959d0de3023b1caf9a76e20999d21fb3eebd2e
Ryan Barouki <ryanbarouki@gmail.com>
2024-05-07 10:06:24 +0100

    fix the small map when resizing

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useState } from 'react';
 import 'leaflet/dist/leaflet.css';
 import styled from 'styled-components';
 import ReactMapGL, { Marker } from 'react-map-gl';
@@ -12,16 +12,16 @@ const Container = styled.div`
 
 const Map2 = styled.div`
   position: absolute;
-  bottom: 0;
-  right: 0;
-  height: 33.33vh;
-  width: 33.33vw;
+  bottom: -40vh;
+  right: -40vw;
+  height: 70vh;
+  width: 70vw;
   transition: all 0.5s ease-in-out;
   z-index: 1;
   
   &:hover {
-    height: 50vh;
-    width: 50vw;
+    bottom: 0;
+    right: 0;
   }
 `;
 
@@ -39,19 +39,6 @@ function Map() {
     longitude: 0,
   })
 
-  const [map2Width, setMap2Width] = useState("33.33vw");
-  const [map2Height, setMap2Height] = useState("33.33vh");
-
-  const handleHover = () => {
-    setMap2Width("50vw");
-    setMap2Height("50vh");
-  };
-
-  const handleHoverEnd = () => {
-    setMap2Width("33.33vw");
-    setMap2Height("33.33vh");
-  };
-
   const handleClick = (event) => {
     console.log(event);
     setMarker({
@@ -60,13 +47,6 @@ function Map() {
     },
     );
   };
-  const key = useMemo(() => map2Height + "-" + map2Width, [map2Height, map2Width]);
-
-  // useEffect(() => {
-  //   console.log(map2Width, map2Height)
-  //   console.log(key)
-  // }, [map2Width, map2Width]
-  // )
 
   return (
     <Container>
@@ -78,23 +58,17 @@ function Map() {
         onViewportChange={setViewport}
       >
       </ReactMapGL>
-      <Map2
-        onMouseOver={handleHover}
-        onMouseLeave={handleHoverEnd}
-      >
+      <Map2 >
         <ReactMapGL
-          key={key}
           onClick={handleClick}
           mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
           initialViewState={viewport}
-          width={map2Width}
-          height={map2Height}
-          // style={{ width: map2Width, height: map2Height }}
+          style={{ width: '100%', height: '100%' }}
           mapStyle="mapbox://styles/ryanbarouki/clvv9mmgd00v101quhx4m3nkv"
           onViewportChange={setViewport}
         >
           <Marker latitude={marker.latitude} longitude={marker.longitude}>
-            <div>something</div>
+            <div>PIN</div>
           </Marker>
         </ReactMapGL>
       </Map2>

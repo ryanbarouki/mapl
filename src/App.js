6142155666da554c5e6e53fbbb00f8dbee2506e1
Ryan Barouki <ryanbarouki@gmail.com>
2024-05-07 10:17:33 +0100

    refactor to separate files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-import Map from './components/Map.js'
+import Map from './components/Map.js';
+import GuessMap from './components/GuessMap.js';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
   return (
-    <Map/>
+    <Container>
+      <Map />
+      <GuessMap />
+    </Container>
   );
 }
 
---
+++ src/components/GuessMap.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import 'leaflet/dist/leaflet.css';
+import styled from 'styled-components';
+import ReactMapGL, { Marker } from 'react-map-gl';
+
+const Map2 = styled.div`
+  position: absolute;
+  bottom: -40vh;
+  right: -40vw;
+  height: 70vh;
+  width: 70vw;
+  transition: all 0.5s ease-in-out;
+  z-index: 1;
+  
+  &:hover {
+    bottom: 0;
+    right: 0;
+  }
+`;
+
+function GuessMap() {
+
+  // Define the resizing code
+  const [viewport, setViewport] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
+  const [marker, setMarker] = useState({
+    latitude: 0,
+    longitude: 0,
+  })
+
+  const handleClick = (event) => {
+    console.log(event);
+    setMarker({
+      longitude: event.lngLat.lng,
+      latitude: event.lngLat.lat,
+    },
+    );
+  };
+
+  return (
+    <Map2 >
+      <ReactMapGL
+        onClick={handleClick}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        initialViewState={viewport}
+        style={{ width: '100%', height: '100%' }}
+        mapStyle="mapbox://styles/ryanbarouki/clvv9mmgd00v101quhx4m3nkv"
+        onViewportChange={setViewport}
+      >
+        <Marker latitude={marker.latitude} longitude={marker.longitude}>
+          <div>PIN</div>
+        </Marker>
+      </ReactMapGL>
+    </Map2>
+  );
+}
+
+export default GuessMap;

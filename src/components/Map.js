89ae38ba6fed565ecc38b0bc0497b471ae02d51d
Ryan Barouki <ryanbarouki@gmail.com>
2024-05-07 22:32:08 +0100

    fix map at zoom level

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,13 +7,15 @@ function Map() {
   const [viewport, setViewport] = useState({
     latitude: 37.7577,
     longitude: -122.4376,
-    zoom: 8
+    zoom: 12,
+    scrollZoom: false,
+    dragPan: false
   });
 
   return (
     <ReactMapGL
+      {...viewport}
       mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      initialViewState={viewport}
       style={{ width: '100%', height: '100%' }}
       mapStyle="mapbox://styles/ryanbarouki/clupsf5gr00vl01r2cqvg81tb"
       onViewportChange={setViewport}
